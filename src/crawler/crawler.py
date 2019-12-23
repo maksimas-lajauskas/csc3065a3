28890cc623ba64f6854b0e68963212113907ccb8
40073762 <623-40073762@users.noreply.gitlab2.eeecs.qub.ac.uk>
2019-12-23 22:39:46 +0000

    random ip gen

---
+++ src/crawler/crawler.py
@@ -0,0 +1,16 @@
+#Superbasic crawler implementation - Maksimas Lajauskas 40073762
+import requests
+import socket
+from random import getrandbits
+from ipaddress import IPv4Address
+
+if True:
+    #step1 pick IP
+    bits = getrandbits(32)
+    addr = IPv4Address(bits)
+    addr_str = str(addr)
+    print(addr_str)
+
+    #check if has record in DB
+
+    #((record exists && time to revisit) || norecord )? continue : next IP
