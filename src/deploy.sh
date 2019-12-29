33613b6992303965d01d1710d004cc4ea7bff68c
40073762 <623-40073762@users.noreply.gitlab2.eeecs.qub.ac.uk>
2019-12-29 11:07:59 +0000

    notes on gcloud service account setup

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+#### FLOW - GCP ONLY:
+#1. Collect and validate data
+#2. Ensure gcloud is pointing to project id passed in the args or fail with message
+#3. If asking for a new service account to be created create one, else use credentials given
+#3.1. If creating -> create, verify with `gcloud list`, assign role and save credentials json
+### account created by using:
+#```
+# gcloud iam service-accounts create [SA-NAME] \
+#    --description "[SA-DESCRIPTION]" \
+#        --display-name "[SA-DISPLAY-NAME]"
+#```
+# on success will output "Created service account [SA-NAME]"
+### role is added by using (more likely):
+#```
+# gcloud projects add-iam-policy-binding $gcp_proj_id \
+#  --member serviceAccount:$gcp_service_account_name@$gcp_proj_id.iam.gserviceaccount.com \
+#    --role roles/$new_role
+#```
+### or role is added by using (less likely, probably this only assigns e.g. jane@example.com ownership of servAcc):
+#```
+# gcloud iam service-accounts add-iam-policy-binding \
+#  my-sa-123@my-project-123.iam.gserviceaccount.com \
+#    --member='user:jane@example.com' --role='roles/editor'
+#```
+#not sure which, if either or if both, first one to work sticks
+### json is retrieved via gcloud as such (manually check json file if all works okay):
+#```
+# gcloud iam service-accounts get-iam-policy \
+#  my-sa-123@my-project-123.iam.gserviceaccount.com \
+#    --format json > policy.json
+#````
+# where policy.json is the expected output path&filename
+### /FLOW
+
 ############### start vars and config capture
 #vars - common
 provider=""
@@ -13,12 +47,13 @@ gcp_use_existing_key_json="False"
 gcp_key_json=""
 gcp_billing_account=""
 gcp_try_create_project="False"
+gcp_service_account_name=""
 
 #build config from args
 case "$1" in
 
     "--help")
-        #todo: help text
+        #!!!TODO!!!: help text
         echo "Sorry fam, you're on your own here lol"
         exit 0
         ;;
@@ -86,7 +121,7 @@ case "$1" in
 
 esac
 
-#todo: validate vars
+#!!!TODO!!!: validate vars, DEPENDS ON: all vars (for at least one provider) being defined with expected values and combinations
 
 ############### end vars and config capture
 
@@ -94,15 +129,15 @@ esac
 
 #builds new gcp config and points config vars to file
 make_new_gcp_creds(){
-#TODO: figure out if feasible and do it, by this point project already exists
+#!!!TODO!!!: figure out if feasible and do it, by this point project already exists, DEPENDS ON: research
 }
 
 ############### end helper functions
 
 ############### start main flow
-#todo: onto building terraform
+#!!!TODO!!!: onto building terraform
 
-#TODO: ALL PROJECT SETUP FOR GCP, LOGIC INCOMPLETE AND MAY BE BROKEN AS IS SO MAY NEED FULL REWRITE
+#!!!TODO!!!: ALL PROJECT SETUP FOR GCP, LOGIC INCOMPLETE AND MAY BE BROKEN AS IS SO MAY NEED FULL REWRITE. DEPENDS ON: R&D
 #set up project
 if [[ "$provider" = "gcp"  ]]
 then
@@ -114,7 +149,8 @@ then
 
     #creating but no billing account? notify and exit
     elif [[ "$gcp_try_create_project" = "True" ]]
-    then #TODO: move this clause out to validation section
+    then
+        #!!!TODO!!!: move this clause out to validation section, DEPENDS ON: flow and vars being established
         echo "Create project $gcp_proj_id $gcp_proj_name  requested but no billing account given"
         exit 1
 
@@ -130,7 +166,8 @@ then
 
         #write credentials file variable
             #using existing credentials? if not, call creds helper function
-        if [[ "$gcp_use_existing_key_json" != "True" ]] #TODO: validate for case where no key given --> SET CREATE CREDS TO FIRE WHEN KEY NOT PASSED IN AND DROP CREATE CREDS BOOL VAR
+        if [[ "$gcp_use_existing_key_json" != "True" ]]
+        #!!!TODO!!!: validate for case where no key given --> SET CREATE CREDS TO FIRE WHEN KEY NOT PASSED IN AND DROP CREATE CREDS BOOL VAR, DEPENDS ON: flow&vars
         then
             make_new_gcp_creds()
 
