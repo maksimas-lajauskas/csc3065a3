b4ab59f53b2b1a1f9ffdfcef483059f9ef9d6275
40073762 <623-40073762@users.noreply.gitlab2.eeecs.qub.ac.uk>
2020-01-13 12:10:51 +0200

    gcp storage tests out

---
+++ src/gcp_storage_interface.py
@@ -0,0 +1,126 @@
+from google.cloud import storage
+import os
+from uuid import uuid1 as uuid
+import json
+
+# from google.cloud import storage
+# import os
+# from uuid import uuid1 as uuid
+# import json
+
+# #init environment
+# keyfile = "keys_debug.json"
+# credvar = "GOOGLE_APPLICATION_CREDENTIALS"
+
+# os.environ[credvar] = os.path.abspath(keyfile)
+# storage_client = storage.Client()
+# os.environ["QSE_STORAGE_BUCKET_NAME"] = "qse-bucket-40073762"
+# bucketname = os.environ["QSE_STORAGE_BUCKET_NAME"]
+
+
+#init environment
+keyfile = "keys.json"
+credvar = "GOOGLE_APPLICATION_CREDENTIALS"
+f = open(keyfile,"w+")
+f.write(os.environ[credvar])
+f.close()
+os.environ[credvar] = os.path.abspath(keyfile)
+
+bucketname = os.environ["QSE_STORAGE_BUCKET_NAME"]
+storage_client = storage.Client()
+bucket = storage_client.bucket(bucketname)
+
+def blob_to_string(blob):
+	try:
+		return blob.download_as_string().decode()
+	except:
+		print(sys.exc_info())
+		return None
+
+def locate_blob_prefix(prefix):
+	for blob in bucket.list_blobs():
+		try:
+			j = json.loads(blob_to_string(blob))
+			if j["header"][:len(prefix)] == prefix:
+				return blob
+		except:
+			continue
+	return None
+
+def delete_blob(header):
+	for blob in bucket.list_blobs():
+		try:
+			j = json.loads(blob_to_string(blob))
+			if j["header"] == header:
+				blob.delete()
+		except:
+			continue
+
+def locate_blob_exact(header):
+	for blob in bucket.list_blobs():
+		try:
+			j = json.loads(blob_to_string(blob))
+			if j["header"] == header:
+				return blob
+		except:
+			continue
+	return None
+
+def ads_query(query_string):
+	results = {}
+	query_string_tokenised = query_string.split(" ")
+	for blob in bucket.list_blobs():
+		try:
+			j = json.loads(blob_to_string(blob))
+			if j["header"][:7] == "advert-":
+				matchscore = 0
+				for i in query_string_tokenised:
+					if j["data"]["keywords"].casefold().find(i.casefold()) >= 0:
+						matchscore += 1
+				if matchscore == 0:
+					continue
+				else:
+					results[j["data"]["url"]] = {"img_height" : j["data"]["img_height"],
+					"img_width": j["data"]["img_width"],
+					"img_mode": j["data"]["img_mode"],
+					"img_bytes": bucket.blob(j["data"]["img_bytes"]).download_as_string()}
+		except:
+			print(sys.exc_info())
+			continue
+	return results
+
+def search_query(query_string):
+	results = {}
+	query_string_tokenised = query_string.split(" ")
+	for blob in bucket.list_blobs():
+		try:
+			j = json.loads(blob_to_string(blob))
+			if j["header"][:8] == "webpage-":
+				matchscore = 0
+				for i in query_string_tokenised:
+					if j["data"]["pagetext"].casefold().find(i.casefold()) >= 0:
+						matchscore += 1
+				if matchscore != len(query_string_tokenised):
+					continue
+				else:
+					results[j["data"]["url"]] = j["data"]["pagetext"][:100].replace("\n"," ").replace("<!--", "").replace("-->", "")+"..."
+		except:
+			continue
+			print(sys.exc_info())
+	return results
+
+#e.g. testdata1 = {"header": "webpage-<url>", "data": {"url": "<...>", "pagetext": "<...>"}, "timestamp": datetime.datetime.utcnow().timestamp() }
+# write(testdata1["header"],json.dumps(testdata1).encode())
+def write(data, header=None): #assumes data (containing redundant header) == bytes
+	blob = None
+	if header is not None:
+		blob = locate_blob_exact(header)
+	if blob is None:
+		blob = bucket.blob(uuid().hex)
+	blob.upload_from_string(data)
+	return blob.name
+#datamodel: advert = {"header": "advert-<...>", "data": {"url": "<...>", "keywords": "<...>", "img_bytes": "<header of image bytes blob>", "img_height": "<...>", "img_width": "<...>", "img_mode": "<...>"}, "timestamp": "<...>" }
+#datamodel: webpage = {"header": "webpage-<url>", "data": {"url": "<...>", "pagetext": "<...>"}, "timestamp": "<...>" }
+#datamodel: ticket = {"header": "ticket-<url>", "data": "<url>", "timestamp": "<...>" }
+#datamodel: image = {"header": "image-<...>", "data": "<bytes>"}
+
