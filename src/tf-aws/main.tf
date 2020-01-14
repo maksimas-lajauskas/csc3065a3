b76118c5647bf0b11e9f46f34a5c6b8e79250ddd
40073762 <623-40073762@users.noreply.gitlab2.eeecs.qub.ac.uk>
2020-01-14 19:25:02 +0200

    bugfix

--- src/tf-aws/main.tf
+++ src/tf-aws/main.tf
@@ -1,7 +1,3 @@
-variable "cluster-name" {
-  default = "terraform-eks-demo"
-  type    = string
-}
 # This data source is included for ease of sample architecture deployment
  # and can be swapped out as necessary.
  data "aws_availability_zones" "available" {
